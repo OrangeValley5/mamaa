02e6366fe5e11a75845f9dc43f0a267a7e6e88f0
orange valley <andywlt78@gmail.com>
2023-11-21 17:53:12 +0100

    u

--- Mail.js
+++ Mail.js
@@ -1,7 +1,7 @@
 
 
 
-const firebaseConfig = {
+/* const firebaseConfig = {
     apiKey: "AIzaSyAYZ9HJYrtdVxJfDPtOBUhUoeeS0LytRXA",
     authDomain: "parte-11191.firebaseapp.com",
     databaseURL: "https://parte-11191-default-rtdb.firebaseio.com",
@@ -10,6 +10,16 @@ const firebaseConfig = {
     messagingSenderId: "117686824843",
     appId: "1:117686824843:web:f0d741f71d8c106140c98e",
     measurementId: "G-JLSRFDZ8NH"
+  }; */
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDAQYVTuKkKTuZr-_2SgeWkdIabDr8xwk8",
+    authDomain: "mamaa-70ef6.firebaseapp.com",
+    projectId: "mamaa-70ef6",
+    storageBucket: "mamaa-70ef6.appspot.com",
+    messagingSenderId: "811519669929",
+    appId: "1:811519669929:web:c2629feed3661214011bb8",
+    measurementId: "G-Y0STER14CH"
   };
 
   //intialize firebase
